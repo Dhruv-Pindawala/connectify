06e7cae9966106ef6e0b5be0d69f040b0d321794
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-06-15 00:02:55 +0530

    server side code complete

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = require("http").createServer(app);
 const cors = require("cors");
 
 const io = require("socket.io")(server, {
-    cors = {
+    cors: {
         origin: "*", //allow access from all origins
         methods: ["GET", "POST"] //access control allow methods are mentioned here
     }
@@ -15,6 +15,22 @@ const PORT = process.env.PORT || 5000; // default port is 5000
 
 app.get("/", (req, res) => {
     res.send("Server is active.");
+});
+
+io.on('connection', (socket) => {
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit("callended");
+    });
+
+    socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit("calluser", { signal: signalData, from, name });
+    });
+
+    socket.on("answercall", (data) => {
+        io.to(data.to).emit("callaccepted", data.signal);
+    });
 });
 
 server.listen(PORT, () => console.log(`Server listening via port ${PORT}`));
