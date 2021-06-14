0c6c79ae75a0632e3b8005a52b435300540ef8d1
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-06-14 22:07:35 +0530

    io server side instance

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const app = require("express")();
 const server = require("http").createServer(app);
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+    cors = {
+        origin: "*", //allow access from all origins
+        methods: ["GET", "POST"] //access control allow methods are mentioned here
+    }
+});
+
+app.use(cors());
