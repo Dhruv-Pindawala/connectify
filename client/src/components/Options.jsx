9cd6cdc5db40340f97bf0e084028d2135f9707cc
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-06-16 01:23:55 +0530

    make a call front end

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -54,6 +54,15 @@ const Options = ( { children } ) => {
                                 <Button variant="contained" color='primary' fullWidth startIcon={<Assignment fontSize="large" />}>Copy your ID</Button>
                             </CopyToClipboard>
                         </Grid>
+                        <Grid item xs={12} md={6} className={classes.padding}>
+                            <Typography gutterBottom variant="h6">Make a Call</Typography>
+                            <TextField label="ID to Call" value={idToCall} onChange={(e) => setIdToCall(e.target.value)} fullWidth />
+                            {callAccepted && !callEnded ? (
+                                <Button variant="contained" color="secondary" startIcon={<PhoneDisabled fontSize="large" />} fullWidth onClick={leaveCall} className={classes.margin} >Hang Up</Button>
+                            ) : (
+                                <Button variant="contained" color="primary" startIcon={<Phone fontSize="large" />} fullWidth onClick={() => callUser(idToCall)} className={classes.margin} >Call</Button>
+                            )}
+                        </Grid>
                     </Grid>
                 </form>
             </Paper>
