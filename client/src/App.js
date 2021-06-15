a548afed757d897b313bf41a85eb7dbb6014e178
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-06-15 12:37:44 +0530

    basic layout with react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Typography, AppBar } from '@material-ui/core';
+
+import VideoPlayer from './components/VideoPlayer';
+import Options from './components/Options';
+import Notifications from './components/Notifications';
 
 const App = () => {
     return (
         <div>
-            Connectify
+            <AppBar position="static" color = "inherit">
+                <Typography variant="h2" align="center">
+                    Connectify
+                </Typography>
+            </AppBar>
+            {/ video player component/}
+            <VideoPlayer />
+            {/ options component, notification component /}
+            <Options>
+                <Notifications />
+            </Options>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
