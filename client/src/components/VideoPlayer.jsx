4a8740bb48921fad7e00211ce85f8e685635fb27
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-06-16 01:00:23 +0530

    user1 account info frontend

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Grid, Typography, Paper } from '@material-ui/core';
+import React, { useContext } from 'react';
+import { Grid, Typography, Paper, useControlled } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { SocketContext } from '../SocketContext';
@@ -26,27 +26,33 @@ const useStyles = makeStyles((theme) => ({
 
 const VideoPlayer = () => {
     const classes = useStyles();
+    const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } = useContext(SocketContext);
 
     return (
         <Grid container className={classes.gridContainer}>
             {/* our video */}
-            <Paper className={classes.paper}>
-                <Grid item xs={12} md={6}>
-                    <Typography variant='h5' gutterBottom>
-                        Name
-                    </Typography>
-                    <video playsInline muted ref={null} autoPlay className={classes.video} />
-                </Grid>
-            </Paper>
+            {stream && (
+                <Paper className={classes.paper}>
+                    <Grid item xs={12} md={6}>
+                        <Typography variant='h5' gutterBottom>
+                            {name || 'Me'}
+                        </Typography>
+                        <video playsInline muted ref={myVideo} autoPlay className={classes.video} />
+                    </Grid>
+                </Paper>
+            )}
             {/* user's video */}
-            <Paper className={classes.paper}>
-                <Grid item xs={12} md={6}>
-                    <Typography variant='h5' gutterBottom>
-                        Name
-                    </Typography>
-                    <video playsInline ref={null} autoPlay className={classes.video} />
-                </Grid>
-            </Paper>
+            {callAccepted && !callEnded && (
+                <Paper className={classes.paper}>
+                    <Grid item xs={12} md={6}>
+                        <Typography variant='h5' gutterBottom>
+                            {call.name || 'Other person'}
+                        </Typography>
+                        <video playsInline ref={userVideo} autoPlay className={classes.video} />
+                    </Grid>
+                </Paper>        
+            )}
+            
         </Grid>
     );
 }
